e4cfdb0e405663283b942fbd027baf038360b9ad
estherjsuh <estherjsuh@gmail.com>
2020-06-01 16:22:57 -0700

    scraps content from total pages

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import requests
 from config import API_KEY
 
-r = requests.get("https://content.guardianapis.com/search?q=corona|covid&api-key={}".format(API_KEY)).json()
+params ={
+    "api-key" : API_KEY,
+    "order-by" : "newest",
+    "page-size" : 200}
+
+url = "https://content.guardianapis.com/search?q=coronavirus|covid"
+
+def get_meta_data():
+    r = requests.get(url, params)
+    return r.json()
+
+r= get_meta_data()
+total_pages = r["response"]["pages"]
+
+results = []
+
+for current_page in range(1, total_pages+1):
+    params['page'] = current_page
+    resp = requests.get(url, params)
+    data = resp.json()
+    results.extend(data['response']['results'])
+    current_page +=1
 
 
-print(r)
+# for i in results:
+#     print(i['webPublicationDate'],i['webTitle'], i['webUrl'])
